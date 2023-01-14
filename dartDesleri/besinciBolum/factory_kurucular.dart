0693d6a3b4d90bb2795092caf874215a67930668
cemalcansanaral <c.can.sanaral@gmail.com>
2023-01-14 14:41:53 +0300

    Factory Constructors

---
+++ dartDesleri/besinciBolum/factory_kurucular.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+  Ogrenci cemal = Ogrenci(1, "Cemal Can");
+  var poyraz = Ogrenci.idSiz("Poyraz");
+  var can = Ogrenci.factoryKurucusu(-5, "Can");
+
+  print(can.id);
+  print(can.isim);
+}
+
+class Ogrenci {
+  int id = 0;
+  String isim = "";
+
+  Ogrenci(this.id, this.isim) {
+    print("Varsayilan Kurucu Calisti...");
+  }
+
+  Ogrenci.idSiz(this.isim) {
+    print("Isimlendirilmis Kurucu Calisti...");
+  }
+
+  factory Ogrenci.factoryKurucusu(int id, String isim) {
+    if (id < 0) {
+      return Ogrenci(5, isim);
+    } else {
+      return Ogrenci(id, isim);
+    }
+  }
+}
